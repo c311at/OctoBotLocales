7a34d4fa01cd03fdef625bef1d3e1b0fbcac1994
Yan <nezd@protonmail.com>
2017-08-29 14:29:30 +0300

    Fixes

--- reader.py
+++ reader.py
@@ -10,14 +10,15 @@ class InvalidLocale(LocaleError):
     pass
 
 def read(path):
-    with open(os.path.normpath(path), encoding="unicode_escape") as f:
+    with open(os.path.normpath(path),) as f:
         data = f.read().split("--- ")[1:]
         if data == []:
             raise InvalidLocale("Invalid Locale file. Doesnt contain any strings")
         readed = {}
         for string in data:
-            readed[string.split("\n")[0]] = "\n".join(string.split("\n")[1:])[:-1]
+            readed[string.split("\n")[0].strip()] = "\n".join(string.split("\n")[1:])[:-1].replace('\\n', '\n')
         return readed
 
 if __name__ == '__main__':
     pprint(read("core/en.octeon_locale"))
+    pprint(read("core/ru.octeon_locale"))
